c5aa7e6a72ff7b74e49ade608f07512533a57704
wontaejang <jwt0212@gmail.com>
2021-09-24 10:10:24 +0900

    py 10872

---
+++ python/10872.py
@@ -0,0 +1,21 @@
+'''
+문제
+> 0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+
+입력
+> 첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.
+
+출력
+> 첫째 줄에 N!을 출력한다.
+'''
+
+num = int(input())
+
+if(num == 0):
+    print(1)
+else:
+    result = 1
+    for x in range(1, num+1):
+        result = result * x
+    
+    print(result)
